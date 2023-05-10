4f32f26dd923b474bd027a913353a64f51564aba
Alejandro Montellano <ualexmont@gmail.com>
2023-05-09 19:55:28 -0600

    Feat - Adding Api features - LocalStorage

--- api-fetch/api-fetch.js
+++ api-fetch/api-fetch.js
@@ -1,6 +1,21 @@
 const btnGetUsers = document.getElementById("btn-get-users");
-const apiURL = "https://reqres.in/api/users?delay=1";
+const apiURL = "https://reqres.in/api/users?delay=3";
 const usersContainer = document.getElementById("users-container");
+const loader = document.getElementById("loader");
+const oneMinuteDelay = 60000;
+let dueFetchTime = 0;
+
+const startLoader = () => {
+    loader.innerHTML = ` <div class="spinner-border text-dark" role="status"></div>`;
+}
+
+const endLoader = () => {
+    loader.innerHTML = ``;
+}
+
+const printUsers = (users) => {
+    usersContainer.innerHTML = users;
+}
 
 const getUsers = async (url) =>{
     try {
@@ -15,11 +30,36 @@ const getUsers = async (url) =>{
 
 btnGetUsers.addEventListener('click', ()=>{
     let listUsers = "";
-   getUsers(apiURL)
-        .then((usersArray) =>{
+    let timeNow = new Date().getTime();
+    
+    if(timeNow <= dueFetchTime){
+        const usersObject = JSON.parse(localStorage.getItem("usersList"));
+        console.log(usersObject);
+        usersObject.forEach((user) => {
+            console.log(user);
+            listUsers += `
+            <tr class="border p-1 my-2">
+                <td scope="row" data-label="ID">${user.id}</td>
+                <td data-label="First Name">${user.first_name}</td>
+                <td data-label="Last Name">${user.last_name}</td>
+                <td data-label="E-Mail">${user.email}</td>
+                <td class="border-bottom-0" data-label="Image"><img class="rounded-circle" width="75" src="${user.avatar}" alt=""></td>
+            </tr>`;
+            printUsers(listUsers);
+            console.log("Ready");
+    });
+    }
+    else{
+        startLoader();
+        getUsers(apiURL)
+            .then((usersArray) =>{
+            
+            dueFetchTime = (new Date().getTime() + oneMinuteDelay);
+            localStorage.setItem("usersList", JSON.stringify(usersArray));
 
-            usersArray.forEach(user => {
-                console.log(user);
+
+            usersArray.forEach((user) => {
+                //console.log(user);
                 listUsers += `
                 <tr class="border p-1 my-2">
                     <td scope="row" data-label="ID">${user.id}</td>
@@ -28,10 +68,12 @@ btnGetUsers.addEventListener('click', ()=>{
                     <td data-label="E-Mail">${user.email}</td>
                     <td class="border-bottom-0" data-label="Image"><img class="rounded-circle" width="75" src="${user.avatar}" alt=""></td>
                 </tr>`;
-                usersContainer.innerHTML = listUsers;
+                printUsers(listUsers);
+                endLoader();
             });
 
         })
+    }
 
 });
 
